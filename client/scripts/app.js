993c0fd1ced5512eefda7fa958c0f9c74b3bfa1e
scottrice10 <scottrice10@gmail.com>
2014-12-29 20:19:09 -0800

    Added checkboxes to filter by rooms.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,7 @@ var message = {
   'roomname': '4chan'
 };
 
-var app = {rooms:{}};
+var app = {rooms:{}, data: [], roomFilters: [], allRooms: {}, currentRooms: {}};
 
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
@@ -19,25 +19,47 @@ app.fetch = function(){
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
+      that.data = data.results;
       $('.messages').html('');
-      for(var i=0;i<data.results.length;i++){
-        var message = data.results[i];
-        // message.fn = '<img src="asdf" onerror="console.log(3)">';
-        $('.messages').append("<li>"
-          + "<span>Created at: " + _.escape(message.createdAt) + "</span></br>"
-          + "<span>Update at: " + _.escape(message.updatedAt) + "</span></br>"
-          + "<span>Function: " + _.escape(message.fn) + "</span></br>"
-          + "<span>Message: " + _.escape(message.text) + "</span></br>"
-          + "<span>Username: " + _.escape(message.username) + "</span></br>"
-          + "<span>Roomname: " + _.escape(message.roomname) + "</span></br>"
-          + "</br></br>"
-          + "</li>");
-        that.rooms[message.roomname] = true;
-      }
+      app.currentRooms = {};
+      data = data.results.filter(function(message, index, array) {
+        var room = _.escape(message.roomname);
+        app.currentRooms[room] = false;
+        if (app.roomFilters.length && app.roomFilters.indexOf(message.roomname) === -1) {
+          return false;
+        }
+        else {
+          $('.messages').append("<li>"
+            + "<span>Created at: " + _.escape(message.createdAt) + "</span></br>"
+            + "<span>Update at: " + _.escape(message.updatedAt) + "</span></br>"
+            + "<span>Function: " + _.escape(message.fn) + "</span></br>"
+            + "<span>Message: " + _.escape(message.text) + "</span></br>"
+            + "<span>Username: " + _.escape(message.username) + "</span></br>"
+            + "<span>Roomname: "  + _.escape(message.roomname)   + "</span></br>"
+            + "</br></br>"
+            + "</li>");
+          that.rooms[message.roomname] = true;
+        }
+        if(index === array.length - 1 && Object.keys(app.allRooms).length !== Object.keys(app.currentRooms).length) {
+          var temp = {};
+          for (var i in app.currentRooms) {
+            temp[i] = app.allRooms[i] || false;;
+          }
+          app.allRooms = temp;
+          delete temp;
+        }
+      });
 
       $('#roomSelect').html('');
-      for (var i in that.rooms) {
-        $('#roomSelect').append("<span>Room: " + _.escape(i) + "</span></br>")
+      for (var i in app.allRooms) {
+        $('#roomSelect').append("<input type='checkbox' id='" + i + "'>" + i + "</br>")
+        .bind('click', function(event){
+          var key = _.escape(event.target.id);
+          console.log(key, event);
+          app.allRooms[key] = !app.allRooms[key];
+          app.roomFilters.push(key);
+          app.fetch();
+        });
       }
     },
     error: function (data) {
@@ -95,4 +117,4 @@ app.init();
 
 setInterval(function(){
   app.fetch();
-},2000);
+},15000);
